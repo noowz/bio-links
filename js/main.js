031401e1577e0c4dbf023da9f42220653e7dd156
Fastxyz <danieeel.dev@gmail.com>
2023-03-30 14:07:38 +0200

    Added share button and other social medias

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@ function switchTheme() {
 
 	html.classList.toggle('lightTheme');
 };
+
+document.querySelector('#shareBtn').addEventListener('click', event => {
+	try {
+		if (navigator.share) {
+			navigator.share({
+				title: 'Fastxyz | DevLinks',
+				url: 'https://fastxyz.github.io/rocketseat-devlinks'
+			});
+
+			console.log('You have shared the link successfully!');
+		} else {
+			alert('Your browser does not support the Web Share API!');
+		};
+	} catch (error) {
+		console.log(`An error occured while using the Web share API!\nError: ${error}`);
+	};
+});
